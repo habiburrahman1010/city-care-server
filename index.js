12437158504d874c260678b6845d663270992b60
Habibur Rahman <mdhabiburrahmanrony991@gmail.com>
2026-02-01 21:12:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+require('dotenv').config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const port = process.env.PORT || 3000
+
+
+
+//middleware 
+
+app.use(express.json());
+app.use(cors());
+
+
+//mongodb uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xqjpkxx.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+
+    try {
+        await client.connect();
+
+        const db = client.db('city_care_db');
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('city care is running!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
